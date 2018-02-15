76a02281efb3587bab3ae4c8bd3931dd1fb214ae
teaperr <teaperr@gmail.com>
2018-02-15 20:59:07 +0100

    Course editor works but not communicate with Api

--- src/admin/models/department-model.ts
+++ src/admin/models/department-model.ts
@@ -1,16 +1,17 @@
 import {CourseModel} from '../../courses/models/course-model'
-export class Department
+export class DepartmentModel
 {
     name: string;
     courses: CourseModel[];
     constructor(name: string)
     {
         this.name=name;
+        this.courses=new Array<CourseModel>();
     }
 
-    static createArrayFromCourseModels(models: CourseModel[]): Department[]
+    static createArrayFromCourseModels(models: CourseModel[]): DepartmentModel[]
     {
-        var departments: Department[]=new Array<Department>();
+        var departments: DepartmentModel[]=new Array<DepartmentModel>();
         models.forEach((course) => {
             let found: boolean=false;
             for(var i=0; i<departments.length; ++i)
@@ -24,7 +25,7 @@ export class Department
             }
             if(!found)
             {
-                departments.push(new Department(course.department));
+                departments.push(new DepartmentModel(course.department));
                 departments[departments.length-1].courses=new Array<CourseModel>();
                 departments[departments.length-1].courses.push(course)
             }
--- src/admin/view-models/courses-editor.ts
+++ src/admin/view-models/courses-editor.ts
@@ -3,18 +3,84 @@ import { UniversityModel } from "../../universities/models/university-model";
 import { autoinject } from "aurelia-framework";
 import { UniversityDetailsModel } from "../../universities/models/university-details-model";
 import { CourseModel } from "../../courses/models/course-model";
-import {Department} from '../models/department-model';
+import { DepartmentModel } from '../models/department-model';
+import { start } from "repl";
 
 
 @autoinject()
 export class CourseEditor {
 
-    departments:Department[];
+    departments: DepartmentModel[];
+    universityName: string;
+    courseHTML: string;
+
+
+
     constructor(private universityService: UniversityService) { }
+
     async activate(params: any) {
         let universityModel: UniversityDetailsModel = await this.universityService.getDetails(params.universityId);
-        let courses: CourseModel[]=universityModel.courses;
-        this.departments=Department.createArrayFromCourseModels(courses);
-        console.log(this.departments);
+        this.universityName = universityModel.name;
+        let courses: CourseModel[] = universityModel.courses;
+        this.departments = DepartmentModel.createArrayFromCourseModels(courses);
     }
+    startEditingDepartment(departmentIndex: number) {
+        var departmentAnchor = document.getElementById(`department-anchor-${departmentIndex}`);
+        if (departmentAnchor.getAttribute('aria-expanded') == "true")
+            departmentAnchor.click();
+
+        var departmentEditor = document.getElementById(`department-editor-${departmentIndex}`);
+        var departmentPanel = document.getElementById(`department-panel-${departmentIndex}`);
+        departmentEditor.setAttribute("style", "display:show");
+        departmentPanel.setAttribute("style", "display:none");
+    }
+    completeEditingDepartment(departmentIndex: number) {
+        var departmentEditor = document.getElementById(`department-editor-${departmentIndex}`);
+        var departmentPanel = document.getElementById(`department-panel-${departmentIndex}`);
+        departmentEditor.setAttribute("style", "display:none");
+        departmentPanel.setAttribute("style", "display:show");
+    }
+    startEditingCourse(course: CourseModel) {
+        var courseEditor = document.getElementById(`course-editor-${course.id}`);
+        var coursePanel = document.getElementById(`course-${course.id}`);
+        courseEditor.setAttribute("style", "display:show");
+        coursePanel.setAttribute("style", "display:none");
+    }
+    completeEditingCourse(course: CourseModel) {
+        var courseEditor = document.getElementById(`course-editor-${course.id}`);
+        var coursePanel = document.getElementById(`course-${course.id}`);
+        courseEditor.setAttribute("style", "display:none");
+        coursePanel.setAttribute("style", "display:show");
+    }
+    async addCourse(departmentIndex: number) {
+        var departmentAnchor = document.getElementById(`department-anchor-${departmentIndex}`);
+        if (departmentAnchor.getAttribute('aria-expanded') == "false")
+            departmentAnchor.click();
+        var courseModel: CourseModel = new CourseModel();
+        courseModel.id = this.generateGuid();
+        await this.departments[departmentIndex].courses.push(courseModel);
+        this.startEditingCourse(courseModel);
+    }
+    private generateGuid(): string {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    async addDepartment()
+    {
+        await this.departments.push(new DepartmentModel(""));
+        this.startEditingDepartment(this.departments.length-1);
+    }
+    deleteDepartment(departmentIndex: number)
+    {
+        //are you sure? TODO
+        this.departments.splice(departmentIndex,1);
+    }
+    deleteCourse(department: DepartmentModel, courseIndex: number)
+    {
+        //are you sure (if course contains review) TODO
+        department.courses.splice(courseIndex,1);
+    }
+
 }
